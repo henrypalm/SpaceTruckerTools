795e627a10743d81cb1e1d382d92c50354302776
henrypalm <rgoodwin130@gmail.com>
2025-07-31 14:33:21 -0400

    Create Yield2SellItemColors.js

---
+++ Yield2SellItemColors.js
@@ -0,0 +1,14 @@
+const Yield2SellItemColors = {
+  "Stileron": "#FFD700",
+  "Quantanium": "#FFD700",
+  "Riccite": "#FFD700",
+  "Taranite": "#006400",
+  "Bexalite": "#006400",
+  "Gold": "#006400",
+  "Diamond": "#006400",
+  "Borase": "#ADD8E6",
+  "Laranite": "#ADD8E6",
+  "Beryl": "#ADD8E6",
+  "Agricium": "#ADD8E6",
+  "Hephaestanite": "#ADD8E6"
+};
