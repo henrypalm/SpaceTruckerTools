44ba7aa25266235e6b4fe27660df342a0338a047
henrypalm <rgoodwin130@gmail.com>
2025-08-13 00:08:49 -0400

    Add files via upload

---
+++ js/CargoManifestWriterItemPrices.js
@@ -0,0 +1,214 @@
+const itemPrices = {
+  "Ship Mining": {
+    "Stileron": {
+      "HICKES RESEARCH OUTPOST, CELLIN, CRUSADER, STANTON": { scu: 33781, cscu: 1, mscu: 1 },
+      "RAYARI KALTAG RESEARCH OUTPOST, CALLIOPE, MICROTECH, STANTON": { scu: 33781, cscu: 1, mscu: 1 },
+      "RAYARI ANVIK RESEARCH OUTPOST, CALLIOPE, MICROTECH, STANTON": { scu: 33781, cscu: 1, mscu: 1 },
+      "ARCCORP MINING AREA 141, DAYMAR, CRUSADER, STANTON": { scu: 32285, cscu: 1, mscu: 1 },
+      "ARCCORP MINING AREA 157, YELA, CRUSADER, STANTON": { scu: 32285, cscu: 1, mscu: 1 },
+      "TERRA MILLS HYDROFARM, CELLIN, CRUSADER, STANTON": { scu: 32003, cscu: 1, mscu: 1 },
+      "THE GOLDEN RIVIERA, BLOOM, PYRO": { scu: 31428, cscu: 1, mscu: 1 },
+      "CANARD VIEW, TERMINUS, PYRO": { scu: 31157, cscu: 1, mscu: 1 },
+      "JACKSON'S SWAP, MONOX, PYRO": { scu: 30344, cscu: 1, mscu: 1 },
+      "FALLOW FIELD, PYRO IV, PYRO V, PYRO": { scu: 29802, cscu: 1, mscu: 1 },
+      "ASHLAND, IGNIS, PYRO V, PYRO": { scu: 28447, cscu: 1, mscu: 1 },
+      "SACREN'S PLOT, PYRO IV, PYRO V, PYRO": { scu: 28447, cscu: 1, mscu: 1 }
+    },
+    "Quantanium": {
+      "ASHLAND, IGNIS, PYRO V, PYRO": { scu: 24013, cscu: 1, mscu: 1 },
+      "CRU L5 MAINTENANCE AREA, CRU L5, STANTON": { scu: 22712, cscu: 1, mscu: 1 },
+      "RUIN STATION, TERMINUS, PYRO": { scu: 21276, cscu: 1, mscu: 1 },
+      "GRIM HEX, YELA, CRUSADER, STANTON": { scu: 20647, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 20647, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 20647, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 20647, cscu: 1, mscu: 1 }
+    },
+    "Riccite": {
+      "ARCCORP MINING AREA 157, YELA, CRUSADER, STANTON": { scu: 24497, cscu: 1, mscu: 1 },
+      "ASHLAND, IGNIS, PYRO V, PYRO": { scu: 24132, cscu: 1, mscu: 1 },
+      "THE GOLDEN RIVIERA, BLOOM, PYRO": { scu: 21662, cscu: 1, mscu: 1 },
+      "SACREN'S PLOT, PYRO IV, PYRO V, PYRO": { scu: 21282, cscu: 1, mscu: 1 },
+      "PATCH CITY, PYRO": { scu: 20522, cscu: 1, mscu: 1 },
+      "DUDLEY & DAUGHTERS, PYRO": { scu: 20522, cscu: 1, mscu: 1 },
+      "ENDGAME, PYRO": { scu: 20522, cscu: 1, mscu: 1 },
+      "GASLIGHT, PYRO": { scu: 20522, cscu: 1, mscu: 1 },
+      "CANARD VIEW, TERMINUS, PYRO": { scu: 20522, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES, PYRO": { scu: 20522, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 20120, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 19744, cscu: 1, mscu: 1 }
+    },
+    "Taranite": {
+      "ORISON, CRUSADER, STANTON": { scu: 9789, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 9551, cscu: 1, mscu: 1 },
+      "GRIM HEX, YELA, CRUSADER, STANTON": { scu: 9223, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 8978, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 8978, cscu: 1, mscu: 1 },
+      "FALLOW FIELD, PYRO IV, PYRO V, PYRO": { scu: 8892, cscu: 1, mscu: 1 },
+      "JACKSON'S SWAP, MONOX, PYRO": { scu: 8575, cscu: 1, mscu: 1 },
+      "CANARD VIEW, TERMINUS, PYRO": { scu: 8575, cscu: 1, mscu: 1 },
+      "THE GOLDEN RIVIERA, BLOOM, PYRO": { scu: 8575, cscu: 1, mscu: 1 },
+      "SACREN'S PLOT, PYRO IV, PYRO V, PYRO": { scu: 8575, cscu: 1, mscu: 1 },
+      "ASHLAND, IGNIS, PYRO V, PYRO": { scu: 8575, cscu: 1, mscu: 1 },
+      "PATCH CITY, PYRO": { scu: 8337, cscu: 1, mscu: 1 }
+    },
+    "Bexalite": {
+      "RAT'S NEST, PYRO": { scu: 7386.5, cscu: 1, mscu: 1 },
+      "CANARD VIEW, TERMINUS, PYRO": { scu: 7178, cscu: 1, mscu: 1 },
+      "THE GOLDEN RIVIERA, BLOOM, PYRO": { scu: 7178, cscu: 1, mscu: 1 },
+      "JACKSON'S SWAP, MONOX, PYRO": { scu: 7178, cscu: 1, mscu: 1 },
+      "SACREN'S PLOT, PYRO IV, PYRO V, PYRO": { scu: 7178, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES, PYRO": { scu: 6979, cscu: 1, mscu: 1 },
+      "ENDGAME, PYRO": { scu: 6979, cscu: 1, mscu: 1 },
+      "DUDLEY & DAUGHTERS, PYRO": { scu: 6979, cscu: 1, mscu: 1 },
+      "PATCH CITY, PYRO": { scu: 6979, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 6630, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 6630, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 6630, cscu: 1, mscu: 1 }
+    },
+    "Gold": {
+      "AREA 18, ARCCORP, STANTON": { scu: 6257, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 6250, cscu: 1, mscu: 1 },
+      "ARC L1, STANTON": { scu: 6225, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 6219, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 6171, cscu: 1, mscu: 1 },
+      "RAT'S NEST, PYRO": { scu: 5974, cscu: 1, mscu: 1 },
+      "GRIM HEX, YELA, CRUSADER, STANTON": { scu: 5546, cscu: 1, mscu: 1 },
+      "FALLOW FIELD, PYRO IV, PYRO V, PYRO": { scu: 5234, cscu: 1, mscu: 1 },
+      "SACREN'S PLOT, PYRO IV, PYRO V, PYRO": { scu: 5234, cscu: 1, mscu: 1 },
+      "ASHLAND, IGNIS, PYRO V, PYRO": { scu: 5234, cscu: 1, mscu: 1 },
+      "THE GOLDEN RIVIERA, BLOOM, PYRO": { scu: 5234, cscu: 1, mscu: 1 },
+      "JACKSON'S SWAP, MONOX, PYRO": { scu: 5234, cscu: 1, mscu: 1 }
+    },
+    "Diamond": {
+      "ARC L5, STANTON": { scu: 5892.5, cscu: 1, mscu: 1 },
+      "HUR L1, STANTON": { scu: 5786, cscu: 1, mscu: 1 },
+      "DUNBORO, MICROTECH, STANTON": { scu: 5713, cscu: 1, mscu: 1 },
+      "RAPPEL, HURSTON, STANTON": { scu: 5713, cscu: 1, mscu: 1 },
+      "PICKERS FIELD, HURSTON, STANTON": { scu: 5713, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES": { scu: 5689, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 5586, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 5454, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 5454, cscu: 1, mscu: 1 },
+      "STARLIGHT SERVICE STATION, PYRO": { scu: 5421, cscu: 1, mscu: 1 },
+      "MAKER'S POINT, HURSTON, STANTON": { scu: 5173, cscu: 1, mscu: 1 },
+      "GRIM HEX, YELA, CRUSADER, STANTON": { scu: 5169, cscu: 1, mscu: 1 }
+    },
+    "Borase": {
+      "ARC L5, STANTON": { scu: 3419, cscu: 1, mscu: 1 },
+      "MIC L3, STANTON": { scu: 3419, cscu: 1, mscu: 1 },
+      "MIC L1, STANTON": { scu: 3419, cscu: 1, mscu: 1 },
+      "HUR L3, STANTON": { scu: 3419, cscu: 1, mscu: 1 },
+      "GASLIGHT, PYRO": { scu: 3256, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, AREA 18": { scu: 3211, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 3175, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES, PYRO": { scu: 3143, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 3122, cscu: 1, mscu: 1 },
+      "DUDLEY & DAUGHTERS, PYRO": { scu: 2058, cscu: 1, mscu: 1 },
+      "PATCH CITY, PYRO": { scu: 2058, cscu: 1, mscu: 1 },
+      "RAT'S NEST, PYRO": { scu: 2058, cscu: 1, mscu: 1 }
+    },
+    "Laranite": {
+      "CRU L1, STANTON": { scu: 2830, cscu: 1, mscu: 1 },
+      "HUR L2, STANTON": { scu: 2830, cscu: 1, mscu: 1 },
+      "HUR L1, STANTON": { scu: 2830, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 2736, cscu: 1, mscu: 1 },
+      "DUDLEY & DAUGHTERS, PYRO": { scu: 2672, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 2663, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES, PYRO": { scu: 2625, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 2584, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 2569, cscu: 1, mscu: 1 },
+      "PATCH CITY, PYRO": { scu: 2531, cscu: 1, mscu: 1 },
+      "STARLIGHT SERVICE STATION, PYRO": { scu: 2531, cscu: 1, mscu: 1 },
+      "GASLIGHT, PYRO": { scu: 2531, cscu: 1, mscu: 1 }
+    },
+    "Beryl": {
+      "HUR L3, STANTON": { scu: 2774, cscu: 1, mscu: 1 },
+      "HUR L5, STANTON": { scu: 2774, cscu: 1, mscu: 1 },
+      "HUR L4, STANTON": { scu: 2774, cscu: 1, mscu: 1 },
+      "MIC L5, STANTON": { scu: 2774, cscu: 1, mscu: 1 },
+      "CRU L5, STANTON": { scu: 2774, cscu: 1, mscu: 1 },
+      "HUR L1, STANTON": { scu: 2774, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 2626, cscu: 1, mscu: 1 },
+      "ENDGAME, PYRO": { scu: 2619, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES, PYRO": { scu: 2596, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON,STANTON": { scu: 2533, cscu: 1, mscu: 1 },
+      "STARLIGHT SERVICE STATION, PYRO": { scu: 2527, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 2518, cscu: 1, mscu: 1 }
+    },
+    "Agricium": {
+      "ARC L4, STANTON": { scu: 2641, cscu: 1, mscu: 1 },
+      "ARC L3, STANTON": { scu: 2622, cscu: 1, mscu: 1 },
+      "HUR L4, STANTON": { scu: 2505, cscu: 1, mscu: 1 },
+      "HUR L3, STANTON": { scu: 2502, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 2502, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 2452, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 2391, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES, PYRO": { scu: 2349, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 2327, cscu: 1, mscu: 1 },
+      "GRIM HEX, YELA, CRUSADER, STANTON": { scu: 2266, cscu: 1, mscu: 1 },
+      "BRIO'S BREAKER YARD, DAYMAR, CRUSADER, STANTON": { scu: 2265, cscu: 1, mscu: 1 },
+      "SAMSON & SON'S SALVAGE CENTER, WALA, ARCCORP, STANTON": { scu: 2265, cscu: 1, mscu: 1 }
+    },
+    "Hephaestanite": {
+      "MIC L4, STANTON": { scu: 2588, cscu: 1, mscu: 1 },
+      "CRU L4, STANTON": { scu: 2588, cscu: 1, mscu: 1 },
+      "MIC L5, STANTON": { scu: 2588, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 2458, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 2363, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 2363, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 2352, cscu: 1, mscu: 1 },
+      "ENDGAME, PYRO": { scu: 2315, cscu: 1, mscu: 1 },
+      "DUDLEY & DAUGHTERS, PYRO": { scu: 2315, cscu: 1, mscu: 1 },
+      "PATCH CITY, PYRO": { scu: 2315, cscu: 1, mscu: 1 },
+      "GASLIGHT, PYRO": { scu: 2315, cscu: 1, mscu: 1 },
+      "STARLIGHT SERVICE STATION, PYRO": { scu: 2315, cscu: 1, mscu: 1 }
+    }
+  },
+  "Handheld and Light Ship Mining": {
+    "Janalite" : {
+      "Seraphim Station, Crusder, Stanton" : { scu: 18559790 }
+    },
+    "Hadanite" : {
+      "No Location data": { scu: 123, cscu: 1, mscu: 1 }
+    },
+    "Feynmaline" : {
+      "No Location data": { scu: 123, cscu: 1, mscu: 1 }
+    },
+    "Aphorite" : {
+      "No Location data": { scu: 123, cscu: 1, mscu: 1 }
+    }
+  },
+  "Salvage": {
+    "Recycled Material Composite": {
+      "SEER'S CANYON, VATRA, PYRO V, PYRO": { scu: 11216, cscu: 1, mscu: 1 },
+      "TERRA MILLS HYDROFARM, CELLIN, CRUSADER, STANTON": { scu: 11097, cscu: 1, mscu: 1 },
+      "LAST LANDINGS, TERMINUS, PYRO": { scu: 10800, cscu: 1, mscu: 1 },
+      "RUSTVILLE, PYRO I, PYRO": { scu: 10800, cscu: 1, mscu: 1 },
+      "THE GOLDEN RIVIERA, BLOOM, PYRO": { scu: 10800, cscu: 1, mscu: 1 },
+      "FALLOW FIELD, PYRO IV, PYRO V, PYRO": { scu: 10800, cscu: 1, mscu: 1 },
+      "ASHLAND, IGNIS, PYRO V, PYRO": { scu: 10800, cscu: 1, mscu: 1 },
+      "RAT'S NEST, PYRO": { scu: 10727, cscu: 1, mscu: 1 },
+      "DUDLEY & DAUGHTERS, PYRO": { scu: 10500, cscu: 1, mscu: 1 },
+      "STARLIGHT SERVICE STATION, PYRO": { scu: 10500, cscu: 1, mscu: 1 },
+      "ROD'S FUEL 'N SUPPLIES, PYRO": { scu: 10500, cscu: 1, mscu: 1 },
+      "ENDGAME, PYRO": { scu: 10500, cscu: 1, mscu: 1 },
+      "Orison, Crusader, Stanton" : { scu: 10424.5, cscu: 1, mscu: 1 },
+      "Providence Platform, Orison, Crusader, Stanton" : { scu: 10339.5, cscu: 1, mscu: 1 },
+      "Grim Hex, Yela, Crusader, Stanton" : { scu: 9265, cscu: 1, mscu: 1 }
+    },
+    "Construction Material": {
+      "HUR L1, STANTON": { scu: 2414.5, cscu: 1, mscu: 1 },
+      "ARC L1, STANTON": { scu: 2414, cscu: 1, mscu: 1 },
+      "HUR L4, STANTON": { scu: 2414, cscu: 1, mscu: 1 },
+      "HUR L3, STANTON": { scu: 2412, cscu: 1, mscu: 1 },
+      "MEGUMI REFUELING, PYRO": { scu: 2300, cscu: 1, mscu: 1 },
+      "NEW CABBAGE, MICROTECH, STANTON": { scu: 2188.51, cscu: 1, mscu: 1 },
+      "RUIN STATION, TERMINUS, PYRO": { scu: 2170.5, cscu: 1, mscu: 1 },
+      "CHECKMATE, PYRO": { scu: 2160, cscu: 1, mscu: 1 },
+      "LORVILLE, HURSTON, STANTON": { scu: 2152, cscu: 1, mscu: 1 },
+      "OBITUARY, BLOOM, PYRO": { scu: 2147, cscu: 1, mscu: 1 },
+      "ORISON, CRUSADER, STANTON": { scu: 2140.51, cscu: 1, mscu: 1 },
+      "AREA 18, ARCCORP, STANTON": { scu: 2124.5, cscu: 1, mscu: 1 }
+    }
+  }
+};
