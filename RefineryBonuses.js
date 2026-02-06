7dcba8df0f018c61864b8731aeb4270861c73698
henrypalm <rgoodwin130@gmail.com>
2026-02-06 15:35:09 -0500

    Add refinery bonuses for Stanton, Pyro, and Nyx

---
+++ RefineryBonuses.js
@@ -0,0 +1,13 @@
+const refineryBonuses = {
+  "Stanton": {
+    "Area 18, ARCCORP, STANTON": 1.05,
+    "Lorville, HURSTON, STANTON": 1.03
+  },
+  "Pyro": {
+    "Rod's Fuel 'N Supplies, PYRO": 1.02,
+    "Jackson's Swap, MONOX, PYRO": 1.01
+  },
+  "Nyx": {
+    "Levski, NYX": 1.04
+  }
+};
