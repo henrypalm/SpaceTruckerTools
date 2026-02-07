bb8d45075ac20e562ce081638731ac86efd2ccb8
henrypalm <rgoodwin130@gmail.com>
2026-02-06 22:20:01 -0500

    Refactor item colors and add new entries and add tiers in comments

--- itemColors.js
+++ itemColors.js
@@ -1,29 +1,53 @@
 // itemColors.js
 const itemColors = {
+  // S tier
   "Stileron": "#FFD700",
-  "Quantanium": "#FFD700",
   "Riccite": "#FFD700",
-  "Taranite": "#006400",
+  "Savrilium": "#FFD700",
+  "Quantanium": "#FFD700",
+  // A tier
+  "Lindinium": "#006400",
   "Bexalite": "#006400",
-  "Gold": "#006400",
-  "Diamond": "#006400",
+  "Taranite": "#006400",
+  // B tier
+  "Diamond": "#ADD8E6",
+  "Gold": "#ADD8E6",
   "Borase": "#ADD8E6",
   "Laranite": "#ADD8E6",
   "Beryl": "#ADD8E6",
   "Agricium": "#ADD8E6",
   "Hephaestanite": "#ADD8E6",
-  "Corundum": "#FF0000",
-  "Quartz": "#FF0000",
-  "Graphite": "#FF0000",
-  "AmioshiPlasma": "#FF0000",
-  "Copper": "#FF0000",
+  // C tier
+  "Ice": "#FF0000",
   "Tungsten": "#FF0000",
+  "Titanium": "#FF0000",
+  "Torite": "#FF0000",
+  "Iron": "#FF0000",
+  "Quartz": "#FF0000",
+  "Corundum": "#FF0000",
+  "Copper": "#FF0000",
   "Aluminum": "#FF0000",
+  "Tin": "#FF0000",
+  "Silicon": "#FF0000",
   "Inert Materials": "#FF0000",
+  // Salvage Items
   "Recycled Material Composite": "#FFD700",
-  "Construction Material": "#ADD8E6",
+  "Construction Material": "#006400",
+  //Roc Minables
+  // S tier
   "Janalite" : "#FFD700",
-  "Hadanite":"#ADD8E6",
+  // A tier
+  "Hadanite":"#006400",
+  // B tier
   "Feynmaline":"#ADD8E6",
-  "Aphorite":"#ADD8E6"
+  // C tier
+  "Aphorite":"#FF0000",
+  // D tier --- maybe find a better color for worthless
+  "Beradom":"##FF0000",
+  "Dolivine":"##FF0000",
+  "Glacosite":"##FF0000",
+  // F tier --- no cost for selling, might have player value for wikelo
+  "Carinite":"##FF0000",
+  "Jaclium":"##FF0000",
+  "Saldynium":"##FF0000" 
 };
