975f531e9b75037134a71efbacccfe5bbf81e855
henrypalm <rgoodwin130@gmail.com>
2026-02-08 19:32:11 -0500

    Create refineryProcesses.js

---
+++ refineryProcesses.js
@@ -0,0 +1,10 @@
+const refineryProcesses = {
+    "Dinyx Solvention ": {
+        yield: 0.45
+    },
+    "Ferron Exchange": {
+        yield: 0.45
+    }
+};
+
+const refineryPercentage = refineryProcesses;
